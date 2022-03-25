d27b3caa05d3ee8412391c1fd8b5e9fa17a6f11b
Abdullah Al Safayet <abdullah.safayet11@gmail.com>
2022-03-25 23:17:35 +0600

    header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div>
+      <h1 className="header">The Fake Shop</h1>
+      <h2 className="">Welcome to our Fake Shop</h2>
+      <h3>Choose Your Desire Laptop</h3>
+    </div>
+  );
+};
+
+export default Header;
